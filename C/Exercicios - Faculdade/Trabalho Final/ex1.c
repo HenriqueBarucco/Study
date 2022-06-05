d29dbf292ebf3252aa4efe2df25093a98654335e
Henrique Barucco <33501527+HenriqueBarucco@users.noreply.github.com>
2022-06-05 18:55:20 -0300

    C add

--- C/Exercicios - Faculdade/Trabalho Final/ex1.c
+++ C/Exercicios - Faculdade/Trabalho Final/ex1.c
@@ -12,22 +12,25 @@ void main()
         printf("Insira a idade do nadador para ser classificado: ");
         scanf("%d", &age);
         fseek(stdin, 0, SEEK_END);
+
+        if (age >= 5 && age <= 7)
+            class = "Infantil A";
+
+        else if (age >= 8 && age <= 10)
+            class = "Infantil B";
+
+        else if (age >= 11 && age <= 13)
+            class = "Juvenil A";
+
+        else if (age >= 14 && age <= 17)
+            class = "Juvenil B";
+
+        else if (age >= 18)
+            class = "Senior";
+
+        else
+            printf("Valor invalido!\n");
     } while (age <= 4);
-
-    if (age >= 5 && age <= 7)
-        class = "Infantil A";
-
-    else if (age >= 8 && age <= 10)
-        class = "Infantil B";
-
-    else if (age >= 11 && age <= 13)
-        class = "Juvenil A";
-
-    else if (age >= 14 && age <= 17)
-        class = "Juvenil B";
-
-    else
-        class = "Senior";
 
     printf("Nadador de classificacao: %s", class);
 }
