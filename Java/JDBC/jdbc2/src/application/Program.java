26017791f9959412dd943affc9fed74d9295c3eb
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-12 19:46:24 -0300

    Java add
    JDBC

---
+++ Java/JDBC/jdbc2/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
