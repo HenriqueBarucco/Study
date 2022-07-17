2014d367b17caa118cd796552cc809ff72b39ca3
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-16 23:45:43 -0300

    Java add
    JavaFX

---
+++ Java/JavaFX/javafx1/src/main/java/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/gui/View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- Java/JavaFX/javafx1/src/main/java/com/example/javafx1/HelloController.java
+++ Java/JavaFX/javafx1/src/main/java/com/example/javafx1/HelloController.java
@@ -6,9 +6,32 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label teste;
+
+    private boolean isArmed = false;
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    protected boolean isArmed() {
+        if (isArmed) {
+            return this.isArmed = false;
+        } else {
+            return this.isArmed = true;
+        }
+    }
+
+    @FXML
+    protected void onToggleButtonClick() {
+        isArmed();
+        if (isArmed) {
+            teste.setText("Ativado!");
+        } else {
+            teste.setText("Desativado!");
+        }
     }
 }
