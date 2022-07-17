0be7af90e4c538cca51adf1c7135b31530866aee
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-17 00:32:46 -0300

    Java add
    JavaFX

--- Java/JavaFX/javafx/src/main/java/com/example/javafx/Main.java
+++ Java/JavaFX/javafx/src/main/java/com/example/javafx/Main.java
@@ -1,4 +1,4 @@
-package application;
+package com.example.javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/gui/View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ Java/JavaFX/javafx/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.javafx to javafx.fxml;
+    exports com.example.javafx;
+}
