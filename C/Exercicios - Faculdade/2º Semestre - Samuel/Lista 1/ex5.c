459e523868c7f92ae5dd5f788925c3b25c5d1114
Henrique Barucco <henrique_barucco@hotmail.com>
2022-09-24 17:53:35 -0300

    Create ex5.c

---
+++ C/Exercicios - Faculdade/2º Semestre - Samuel/Lista 1/ex5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main() {
+  printf("char = Tamanho: %d\n", sizeof(char));
+  printf("unsigned char = Tamanho: %d\n", sizeof(unsigned char));
+  printf("signed char = Tamanho: %d\n", sizeof(signed char));
+  printf("int = Tamanho: %d\n", sizeof(int));
+  printf("unsigned int = Tamanho: %d\n", sizeof(unsigned int));
+  printf("signed int = Tamanho: %d\n", sizeof(signed int));
+  printf("short int = Tamanho: %d\n", sizeof(short int));
+  printf("unsigned short int = Tamanho: %d\n", sizeof(unsigned short int));
+  printf("signed short int = Tamanho: %d\n", sizeof(signed short int));
+  printf("long int = Tamanho: %d\n", sizeof(long int));
+  printf("signed long int = Tamanho: %d\n", sizeof(signed long int));
+  printf("unsigned long int = Tamanho: %d\n", sizeof(unsigned long int));
+  printf("float = Tamanho: %d\n", sizeof(float));
+  printf("double = Tamanho: %d\n", sizeof(double));
+  printf("long double = Tamanho: %d\n", sizeof(long double));
+}
