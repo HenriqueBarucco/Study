cd64ffef9c5722971bb7106bd38d22200afd482b
Henrique Barucco <henrique_barucco@hotmail.com>
2022-11-27 18:04:05 -0300

    Configuração

--- Java/Alura/MS na Prática - Implementando com Java e Spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ Java/Alura/MS na Prática - Implementando com Java e Spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -19,6 +19,7 @@ public class PagamentoService {
     @Autowired
     private PagamentoRepository repository;
 
+    @Autowired
     private ModelMapper modelMapper;
 
     public Page<PagamentoDto> obterTodos(Pageable paginacao) {
