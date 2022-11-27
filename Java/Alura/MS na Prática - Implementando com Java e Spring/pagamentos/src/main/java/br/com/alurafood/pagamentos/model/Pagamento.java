b293953ed8c162d25a5d11319732467c30349ecc
Henrique Barucco <henrique_barucco@hotmail.com>
2022-11-27 18:07:06 -0300

    Update Pagamento.java

--- Java/Alura/MS na Prática - Implementando com Java e Spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
+++ Java/Alura/MS na Prática - Implementando com Java e Spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/model/Pagamento.java
@@ -32,15 +32,12 @@ public class Pagamento {
     @Size(max = 100)
     private String nome;
 
-    @NotBlank
     @Size(max = 19)
     private String numero;
 
-    @NotBlank
     @Size(max = 7)
     private String expiracao;
 
-    @NotBlank
     @Size(min = 3, max = 3)
     private String codigo;
 
