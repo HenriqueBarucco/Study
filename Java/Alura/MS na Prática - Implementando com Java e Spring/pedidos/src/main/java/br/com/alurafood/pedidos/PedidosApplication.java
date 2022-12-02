040a55657ee8b5e16ea4801932131bdf220a311e
Henrique Barucco <henrique_barucco@hotmail.com>
2022-12-01 21:55:14 -0300

    Algo ai

--- Java/Alura/MS na Prática - Implementando com Java e Spring/pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ Java/Alura/MS na Prática - Implementando com Java e Spring/pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -2,10 +2,8 @@ package br.com.alurafood.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class PedidosApplication {
 
 	public static void main(String[] args) {
