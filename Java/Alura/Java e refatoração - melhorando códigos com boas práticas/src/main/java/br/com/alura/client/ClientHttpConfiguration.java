9ed3acd84938b20daa767eb983243194a9aa40cf
Henrique Barucco <henrique_barucco@hotmail.com>
2023-10-12 17:22:23 -0300

    feat: modulo 2

---
+++ Java/Alura/Java e refatoração - melhorando códigos com boas práticas/src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -0,0 +1,34 @@
+package br.com.alura.client;
+
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClientHttpConfiguration {
+
+    public HttpResponse<String> dispararRequisicaoGet(String uri) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> dispararRequisicaoPost(String uri, JsonObject json) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
---
+++ Java/Alura/Java e refatoração - melhorando códigos com boas práticas/src/main/java/br/com/alura/service/AbrigoService.java
@@ -0,0 +1,62 @@
+package br.com.alura.service;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class AbrigoService {
+
+    private ClientHttpConfiguration client;
+
+    public AbrigoService(ClientHttpConfiguration client) {
+        this.client = client;
+    }
+
+    public void listarAbrigo() throws IOException, InterruptedException {
+        String uri = "http://localhost:8080/abrigos";
+        HttpResponse<String> response = client.dispararRequisicaoGet(uri);
+        String responseBody = response.body();
+        JsonArray jsonArray = JsonParser.parseString(responseBody).getAsJsonArray();
+        System.out.println("Abrigos cadastrados:");
+        for (JsonElement element : jsonArray) {
+            JsonObject jsonObject = element.getAsJsonObject();
+            long id = jsonObject.get("id").getAsLong();
+            String nome = jsonObject.get("nome").getAsString();
+            System.out.println(id +" - " +nome);
+        }
+    }
+
+    public void cadastrarAbrigo() throws IOException, InterruptedException {
+        System.out.println("Digite o nome do abrigo:");
+        String nome = new Scanner(System.in).nextLine();
+        System.out.println("Digite o telefone do abrigo:");
+        String telefone = new Scanner(System.in).nextLine();
+        System.out.println("Digite o email do abrigo:");
+        String email = new Scanner(System.in).nextLine();
+
+        JsonObject json = new JsonObject();
+        json.addProperty("nome", nome);
+        json.addProperty("telefone", telefone);
+        json.addProperty("email", email);
+
+        String uri = "http://localhost:8080/abrigos";
+
+        HttpResponse<String> response = client.dispararRequisicaoPost(uri, json);
+
+        int statusCode = response.statusCode();
+        String responseBody = response.body();
+        if (statusCode == 200) {
+            System.out.println("Abrigo cadastrado com sucesso!");
+            System.out.println(responseBody);
+        } else if (statusCode == 400 || statusCode == 500) {
+            System.out.println("Erro ao cadastrar o abrigo:");
+            System.out.println(responseBody);
+        }
+    }
+}
