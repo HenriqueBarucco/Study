68bf6134172502b13d89a5f8a6d79f9f0d7aafc7
Henrique Barucco <henrique_barucco@hotmail.com>
2023-10-17 22:07:53 -0300

    feat(CalculadoraProbabilidadeAdocaoTest.java): adiciona testes para calcular a probabilidade de adoção de um pet
    Adiciona dois testes na classe CalculadoraProbabilidadeAdocaoTest para calcular a probabilidade de adoção de um pet. O primeiro teste (cenario01) simula um pet com 4 anos de idade e 4kg, e espera-se que a probabilidade de adoção seja ALTA. O segundo teste (cenario02) simula um pet com 15 anos de idade e 4kg, e espera-se que a probabilidade de adoção seja MEDIA.

---
+++ Java/Alura/Boas práticas de programação - melhore o código de uma API Java/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,63 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void cenario01() {
+        // idade 4 anos e 4kg
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo 1",
+                "5516990000000",
+                "henrique@email.com"
+        ));
+
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Gato 1",
+                "Siames",
+                4,
+                "Branco",
+                4.0f
+        ), abrigo);
+
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidadeAdocao = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidadeAdocao);
+    }
+
+    @Test
+    void cenario02() {
+        // idade 15 anos e 4kg
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo 1",
+                "5516990000000",
+                "henrique@email.com"
+        ));
+
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Gato 1",
+                "Siames",
+                15,
+                "Branco",
+                4.0f
+        ), abrigo);
+
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidadeAdocao = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.MEDIA, probabilidadeAdocao);
+    }
+}
