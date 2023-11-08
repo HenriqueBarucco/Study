8e5ba46ea6bd3f7120836de3b47134c75fbc44ef
Henrique Barucco <henrique_barucco@hotmail.com>
2023-11-08 17:01:16 -0300

    feat(AdocaoServiceTest.java): adiciona teste para solicitar adoção
    Adiciona um novo arquivo de teste para a classe AdocaoService. O teste `deveriaSalvarAdocaoAoSolicitar` verifica se a adoção é salva corretamente ao solicitar uma adoção. O teste `deveriaChamarValidadoresDeAdocaoAoSolicitar` verifica se os validadores de adoção são chamados corretamente ao solicitar uma adoção.

---
+++ Java/Alura/Boas práticas de programação - melhore o código de uma API Java/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,99 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+
+    @InjectMocks
+    private AdocaoService service;
+
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador1;
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    private SolicitacaoAdocaoDto dto;
+
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar() {
+        this.dto = new SolicitacaoAdocaoDto(10L, 20L, "Motivo");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
+        service.solicitar(dto);
+
+        then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+
+        Assertions.assertEquals(pet, adocaoSalva.getPet());
+        Assertions.assertEquals(tutor, adocaoSalva.getTutor());
+        Assertions.assertEquals(dto.motivo(), adocaoSalva.getMotivo());
+    }
+
+    @Test
+    void deveriaChamarValidadoresDeAdocaoAoSolicitar() {
+        this.dto = new SolicitacaoAdocaoDto(10L, 20L, "Motivo");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
+        validacoes.add(validador1);
+        validacoes.add(validador2);
+
+        service.solicitar(dto);
+
+        BDDMockito.then(validador1).should().validar(dto);
+        BDDMockito.then(validador2).should().validar(dto);
+    }
+
+}
