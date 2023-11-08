392800de8fef179e10e8f3776c72dfd4c94f9c5d
Henrique Barucco <henrique_barucco@hotmail.com>
2023-11-08 16:25:35 -0300

    fix(ValidacaoPetDisponivelTest.java): corrige testes de validação de pet disponível
    Os testes foram corrigidos para utilizar o método `getReferenceById` do `PetRepository` e verificar se o pet está adotado ou não. Caso o pet esteja adotado, é esperada uma exceção do tipo `ValidacaoException`.

--- Java/Alura/Boas práticas de programação - melhore o código de uma API Java/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ Java/Alura/Boas práticas de programação - melhore o código de uma API Java/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,22 +1,44 @@
 package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    private ValidacaoPetDisponivel validacao;
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
 
     @Test
     void deveriaPermitirSolicitacaoDeAdocaoPet() {
-
-        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
-                7l,
-                2l,
-                "Motivo qualquer"
-        );
-
-        ValidacaoPetDisponivel validacao = new ValidacaoPetDisponivel();
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
 
         Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoPet() {
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        Assertions.assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
     }
 }
