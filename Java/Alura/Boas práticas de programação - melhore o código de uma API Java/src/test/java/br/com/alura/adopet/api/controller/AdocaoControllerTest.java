91f655d6cf7e744c1830d0dd1ccdb9f8a7a491b0
Henrique Barucco <henrique_barucco@hotmail.com>
2023-11-08 17:19:24 -0300

    feat(AdocaoControllerTest.java): adiciona testes para a classe AdocaoController
    Adiciona testes para verificar se a solicitação de adoção retorna o código de status correto. Um teste verifica se a solicitação com erros retorna o código 400 e o outro teste verifica se a solicitação sem erros retorna o código 200.

---
+++ Java/Alura/Boas práticas de programação - melhore o código de uma API Java/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,56 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService service;
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitarDeAdocaoComErros() throws Exception {
+        String json = "{}";
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        Assertions.assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaSolicitarDeAdocaoSemErros() throws Exception {
+        String json = """
+                {
+                        "idPet": 1,
+                        "idTutor": 1,
+                        "motivo": "Motivo qualquer"
+                }
+                """;
+
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        Assertions.assertEquals(200, response.getStatus());
+    }
+}
